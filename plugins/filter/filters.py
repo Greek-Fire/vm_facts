636f11d59adcb8224ac69eff71c247416efbbc1a
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2023-03-15 15:19:54 -0400

    Add function to make sure ip matches the provided network

--- plugins/filter/filters.py
+++ plugins/filter/filters.py
@@ -6,7 +6,8 @@ from ansible.errors import AnsibleFilterError
 class FilterModule(object):
     def filters(self):
         return {
-            'get_ip_validation': self.get_ip_validation
+            'get_ip_validation': self.get_ip_validation,
+            'match_network': self.match_network
         }
 
     def get_ip_validation(self, ip_address, network_address, gateway_address):
@@ -14,28 +15,50 @@ class FilterModule(object):
         This filter returns True if the provided IP address is valid for the provided network,
         is not a broadcast address, and is not the same as the gateway address, otherwise False
         """
-        ip = ipaddress.IPv4Address(ip_address)
         try:
             if '/' in network_address:
                 network = ipaddress.IPv4Network(network_address)
             else:
                 raise AnsibleFilterError(
-                    "Network address '{}' is missing the netmask".format(network_address)
+                    f"Network address {network_address} is missing the netmask"
                 )
-        except ValueError:
+            gw = ipaddress.IPv4Address(gateway_address)
+            ip = ipaddress.IPv4Address(ip_address)
+        except ipaddress.AddressValueError:
             raise AnsibleFilterError(
-                "Network address '{}' is not a valid IP network address".format(network_address)
+                f"Invalid IP address: {ip_address} or {gw}"
             )
-        gateway = ipaddress.IPv4Address(gateway_address)
 
         is_valid = True
         if ip == network.network_address:
             is_valid = False
         elif ip == network.broadcast_address:
             is_valid = False
-        elif ip == gateway:
+        elif ip == gw:
             is_valid = False
         elif not network.overlaps(ipaddress.IPv4Network(ip_address + '/32')):
             is_valid = False
 
         return is_valid
+    
+    def match_network(self, ip_address, network_list):
+        '''
+        # Convert the input IP address to an IPv4Address object
+        '''
+        try:
+            ip = ipaddress.IPv4Address(ip_address)
+
+        except ipaddress.AddressValueError:
+            raise ValueError(f"Invalid IP address: {ip}")
+        
+        for network_address in network_list:
+            if '/' in network_address:
+                network = ipaddress.IPv4Network(network_address)
+            else:
+                raise AnsibleFilterError(
+                    f"Network address {network_address} is missing the netmask"
+                )
+            network = ipaddress.IPv4Network(network_address)
+            if ip in network:
+                return str(network)
+        return False
