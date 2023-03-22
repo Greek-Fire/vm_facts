0773f69a9f7096978477d07ddae9eedbd50c87b9
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2023-03-22 10:29:06 -0400

    added template finder

--- modules/get_template.py
+++ modules/get_template.py
@@ -2,12 +2,12 @@
 
 DOCUMENTATION = '''
 ---
-module: get_folder
+module: get_template
 version_added: 1.0.0
-short_description: Get a folder in vCenter by name and return its path.
+short_description: Get a template in vCenter by name.
 description:
-  - This module uses the vCenter API to retrieve a folder by name and return its full path.
-  - You can optionally provide the name of the folder's parent folder to help narrow the search.
+  - This module uses the vCenter API to retrieve a template by name.
+  - Returns the full path of the template.
 author:
   - "Louis Tiches"
 options:
@@ -28,29 +28,24 @@ options:
     required: false
     default: false
     type: bool
-  folder_name:
-    description: The name of the folder to retrieve.
+  template_name:
+    description: The name of the template to retrieve.
     required: true
-    type: str
-  parent_folder_name:
-    description: The name of the parent folder to search under. If not provided, search from the root folder.
-    required: false
     type: str
 '''
 
 EXAMPLES = '''
-- name: Get vCenter folder path
-  get_folder:
+- name: Get vCenter template path
+  get_template:
     vcenter: vcenter.example.com
     username: admin
     password: password
-    folder_name: my_folder
-    parent_folder_name: my_parent_folder
-  register: vcenter_folder
+    template_name: my_template
+  register: vcenter_template
 
-- name: Debug vCenter folder path
+- name: Debug vCenter template path
   debug:
-    var: vcenter_folder.ansible_facts.vcenter_folder
+    var: vcenter_template.ansible_facts.vcenter_template
 '''
 
 from ansible.module_utils.basic import AnsibleModule
@@ -63,13 +58,12 @@ def main():
         username=dict(type='str', required=True),
         password=dict(type='str', required=True, no_log=True),
         disable_ssl_verification=dict(type='bool', default=False),
-        folder_name=dict(type='str', required=True),
-        parent_folder_name=dict(type='str', required=False),
+        template_name=dict(type='str', required=True),
     )
 
     result = dict(
         changed=False,
-        vcenter_folder={},
+        vcenter_template={},
         error="",
     )
 
@@ -81,17 +75,17 @@ def main():
     if module.check_mode:
         module.exit_json(**result)
 
-    # Retrieve vCenter folder path
+    # Retrieve vCenter template path
     vcenter_facts = VcenterFacts(
         module.params['vcenter'],
         module.params['username'],
         module.params['password'],
         module.params['disable_ssl_verification'],
     )
-    folder_path = vcenter_facts.get_folder_path(
-        module.params['folder_name']
+    template_path = vcenter_facts.get_template_path(
+        module.params['template_name']
     )
-    result['vcenter_folder'] = folder_path
+    result['vcenter_template'] = template_path
     module.exit_json(**result)
 
 
