0fcf245547c25ca55dacd3dda807279faed0dcd2
bob <57952350+TheRedGreek@users.noreply.github.com>
2023-04-03 13:54:56 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,36 @@
-def get_datastore_with_most_space_in_cluster(self, datastore_cluster_name):
+def get_datastore_with_most_space_in_cluster(self, datastore_cluster_name, compute_cluster_name):
     """
-    Find the datastore with the most available storage in the specified datastore cluster.
-    If a datacenter is specified, only consider datastores in that datacenter.
+    Find the datastore with the most available storage in the specified datastore cluster and compute cluster.
     """
     datacenters = self.get_datacenters()
     datastore_cluster = None
+    compute_cluster = None
 
     for datacenter in datacenters:
-        for cluster in datacenter.hostFolder.childEntity:
-            if isinstance(cluster, vim.ClusterComputeResource) and cluster.name == datastore_cluster_name:
+        for cluster in datacenter.datastoreFolder.childEntity:
+            if isinstance(cluster, vim.StoragePod) and cluster.name == datastore_cluster_name:
                 datastore_cluster = cluster
                 break
-        if datastore_cluster:
+        for cluster in datacenter.hostFolder.childEntity:
+            if isinstance(cluster, vim.ComputeResource) and cluster.name == compute_cluster_name:
+                compute_cluster = cluster
+                break
+        if datastore_cluster and compute_cluster:
             break
 
     if not datastore_cluster:
-        raise Exception(f"No cluster found with the name '{datastore_cluster_name}'")
+        raise Exception(f"No datastore cluster found with the name '{datastore_cluster_name}'")
+    if not compute_cluster:
+        raise Exception(f"No compute cluster found with the name '{compute_cluster_name}'")
 
-    datastores = datastore_cluster.datastore
+    datastores = []
+    for host in compute_cluster.host:
+        for datastore in host.datastore:
+            if datastore in datastore_cluster.childEntity:
+                datastores.append(datastore)
 
     if not datastores:
-        raise Exception("No datastores found in the specified cluster")
+        raise Exception("No datastores found in the specified datastore cluster and compute cluster")
 
     # Find the datastore with the most free space
     max_datastore = max(datastores, key=lambda x: x.summary.freeSpace)
@@ -28,6 +38,7 @@ def get_datastore_with_most_space_in_cluster(self, datastore_cluster_name):
     return {
         'name': max_datastore.name,
         'datastore_cluster': datastore_cluster_name,
+        'compute_cluster': compute_cluster_name,
         'free_space': max_datastore.summary.freeSpace,
         'total_space': max_datastore.summary.capacity
     }
